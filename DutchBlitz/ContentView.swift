01f2f479b29a5fe97797f8704fbf295b8d1e055c
annacwu <annacwu04@gmail.com>
2024-04-24 11:17:32 -0700

    Initial Commit

---
+++ DutchBlitz/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DutchBlitz
+//
+//  Created by Anna Wu on 4/24/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DutchBlitz/DutchBlitzApp.swift
@@ -0,0 +1,17 @@
+//
+//  DutchBlitzApp.swift
+//  DutchBlitz
+//
+//  Created by Anna Wu on 4/24/24.
+//
+
+import SwiftUI
+
+@main
+struct DutchBlitzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
